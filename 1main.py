d1a88749298a0e940ffd49a59df5e01385674f66
eshansamiru <95476420+eshansamiru@users.noreply.github.com>
2021-12-22 02:42:04 -0800

    Create 1main.py

---
+++ 1main.py
@@ -0,0 +1,6 @@
+-----> Building on the Heroku-20 stack
+-----> Determining which buildpack to use for this app
+ !     No default language could be detected for this app.
+			HINT: This occurs when Heroku cannot detect the buildpack to use for this application automatically.
+			See https://devcenter.heroku.com/articles/buildpacks
+ !     Push failed
